41cf6bbf3e5f5aa9bc97db80033dc1d03a84568a
Sheel Patel <sheelpatel5252@gmail.com>
2024-05-06 20:27:43 -0400

    converting spritanimation to regular sprit

--- player/mainCharacter.gd
+++ player/mainCharacter.gd
@@ -17,6 +17,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 # Declare _double_jump_charged at the top of the script
 var _double_jump_charged = false
 var doubleSpace := false 
+var last_facing_direction = 1  # 1 for right, -1 for left
 
 func _physics_process(delta):
 	if is_on_floor():
@@ -26,12 +27,14 @@ func _physics_process(delta):
 	elif Input.is_action_just_released("jump" + action_suffix) and velocity.y < 0.0:
 		# The player let go of jump early, reduce vertical momentum.
 		velocity.y *= 0.6
-	
+
 	# Handle vertical and horizontal movements.
 	velocity.y = minf(TERMINAL_VELOCITY, velocity.y + gravity * delta)
-	var direction := Input.get_axis("left" + action_suffix, "right" + action_suffix) * SPEED
-	velocity.x = move_toward(velocity.x, direction, ACCELERATION_SPEED * delta)
-	
+	var direction := Input.get_axis("left" + action_suffix, "right" + action_suffix)
+	if direction != 0:
+		last_facing_direction = sign(direction)  # Update last known direction
+	velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION_SPEED * delta)
+
 	# Handle animations and direction.
 	if abs(velocity.x) > 1:
 		sprite_2d.animation = "walking"
@@ -41,10 +44,10 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("attack"):
 		animation.play("attacking")
 	
-	# Perform movement and handle sprite flipping based on direction.
+	# Perform movement and handle sprite flipping based on last facing direction.
 	move_and_slide()
-	sprite_2d.flip_h = velocity.x < 0
-	
+	sprite_2d.flip_h = last_facing_direction == -1
+
 func try_jump() -> void:
 	if is_on_floor():
 		jump_sound.pitch_scale = 1.0
